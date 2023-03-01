7ed68e8af0a96071270df1c16ea631460b137847
Felipe Villarinho <felipe-alves.lima@serpro.gov.br>
2023-03-01 16:18:19 -0300

    Part 1 - Guess Input

---
+++ src/components/GuessInput/GuessInput.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+function GuessInput() {
+  const [guess, setGuess] = React.useState("");
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log("Guess submitted:", guess);
+    setGuess("");
+  }
+
+  return (
+    <form className="guess-input-wrapper" onSubmit={handleSubmit}>
+      <label htmlFor="guess-input">Enter guess:</label>
+      <input
+        required
+        id="guess-input"
+        type="text"
+        value={guess}
+        maxLength="5"
+        pattern="[a-zA-Z]{5}"
+        title="5 letter word"
+        onChange={(e) =>
+          setGuess(e.target.value ? e.target.value.toUpperCase() : "")
+        }
+      />
+    </form>
+  );
+}
+
+export default GuessInput;
